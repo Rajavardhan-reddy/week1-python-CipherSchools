76a5f6ac253a33b6cc811ece8eea046f703be466
rajavardhan <Rajavardhan.12221591@lpu.in>
2022-12-02 00:02:16 +0530

    initial commit

---
+++ class2-dataTypes-CipherSchools.py
@@ -0,0 +1,11 @@
+# to print the syntax is given below
+print("Hello World")
+#variables
+a = 100
+b = "hello"
+c = True
+print(type(a))
+print(type(b))
+print(type(c))
+#camelCased naming system
+isTrue = True
